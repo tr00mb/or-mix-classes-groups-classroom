3184fe54d8e209fc4de16692f69927cf6904ecb9
Yohann Agostini <44064161+tr00mb@users.noreply.github.com>
2019-06-07 17:26:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -41,9 +41,9 @@ def main():
 
   p == x * y, which is the same as p <=> x and y
   """
-  all_eleves=[('eleve1',0,''),('eleve2',0,''),('eleve3',1,'*'),('eleve4',1,'')]
-  all_groups=range(2)
-  all_rooms=range(2)
+  all_eleves=[('eleve1',0,''),('eleve2',0,'*'),('eleve3',0,''),('eleve4',0,''),('eleve11',1,''),('eleve12',1,'*'),('eleve13',1,''),('eleve14',1,''),('eleve21',2,''),('eleve22',2,''),('eleve23',2,''),('eleve24',2,'')]
+  all_groups=range(4)
+  all_rooms=range(3)
 
   model = cp_model.CpModel()
 
@@ -63,30 +63,39 @@ def main():
       for e in all_eleves:
         p[(g,r,e)] = model.NewBoolVar('p')
 
+  # each pupil is in one group
   for e in all_eleves:
         model.Add(sum(groups[(e,g)] for g in all_groups) == 1)
 
+  # groups are composed from pupils from different classes
   for c in all_rooms:
         for g in all_groups:
             model.Add(sum(groups[(e,g)] for e in all_eleves if e[1] == c ) <= 1)
 
+  # groups of 3
   for g in all_groups:
-    model.Add(sum(groups[(e,g)] for e in all_eleves) == 2)
+    model.Add(sum(groups[(e,g)] for e in all_eleves) == 3)
 
+  # at least 1 group per room
   for r in all_rooms:
-        model.Add(sum(rooms[(g,r)] for g in all_groups) == 1)
+        model.Add(sum(rooms[(g,r)] for g in all_groups) >= 1)
   
+  # each group is in one room
   for g in all_groups:
     model.Add(sum(rooms[(g,r)] for r in all_rooms) == 1)
 
-  for r in all_rooms:
-    for e in all_eleves:
-      if e[2]=='*':
-        # x and y implies p, rewrite as not(x and y) or p
-        model.AddBoolOr([groups[(e,g)].Not(), rooms[(g,r)].Not(), p[(g,r,e)]])
-        # p implies x and y, expanded into two implication
-        model.AddImplication(p[(g,r,e)], groups[(e,g)])
-        model.AddImplication(p[(g,r,e)], rooms[(g,r)])
+  for e in all_eleves:
+      for r in all_rooms:
+        for g in all_groups:
+          # x and y implies p, rewrite as not(x and y) or p
+          model.AddBoolOr([groups[(e,g)].Not(), rooms[(g,r)].Not(), p[(g,r,e)]])
+          # p implies x and y, expanded into two implication
+          model.AddImplication(p[(g,r,e)], groups[(e,g)] )
+          model.AddImplication(p[(g,r,e)], rooms[(g,r)] )
+
+ # for r in all_rooms:
+  #  for g in all_groups:
+   #   model.Add( sum(p[(g,r,e)] for e in all_eleves if e[2] == '*') == 0 )
 
   # Create a solver and solve.
   solver = cp_model.CpSolver()
@@ -97,3 +106,13 @@ def main():
   solution_printer = PartialSolutionPrinter(
         groups, rooms, all_eleves, all_groups, all_rooms, few_sols)
   solver.SearchForAllSolutions(model, solution_printer)
+
+  # Statistics.
+  print()
+  print('Statistics')
+  print('  - conflicts       : %i' % solver.NumConflicts())
+  print('  - branches        : %i' % solver.NumBranches())
+  print('  - wall time       : %f s' % solver.WallTime())
+  print('  - solutions found : %i' % solution_printer.solution_count())
+
+main()
